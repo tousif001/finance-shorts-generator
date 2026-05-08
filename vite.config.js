a77918fa98737f2f3c5899d81f85d46097a53656
Touseef <89360587+tousif001@users.noreply.github.com>
2026-05-08 06:12:41 +0530

    add niche and visual style selectors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
